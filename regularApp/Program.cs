f87ea5600fae9fad0a177d9e5cb8deb3f2b47b23
safcsp dev <dev@test.com>
2024-01-24 23:08:03 +0400

    Add project files.

---
+++ regularApp/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Please insert your test!");
+var input = Console.ReadLine();
+
+string expressionPhone = @"\b[0-9]{8}\b";
+
+if(input != "")
+{
+    var phone = Regex.Match(input, expressionPhone);
+
+    var phoneValue = "";
+
+    if (phone.Success)
+    {
+
+        phoneValue = phone.Value;
+    }
+    else
+    {
+        phoneValue = "it can not found Phone Number";
+    }
+
+
+    Console.WriteLine(" Phone : " + phoneValue);
+
+}
+else
+{
+    Console.WriteLine("You should write something");
+}
